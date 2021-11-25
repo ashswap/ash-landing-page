10086fc2158969c03202d635589592c2b267c60c
Hieu Phan <hieutrunglephan@gmail.com>
2021-11-25 10:39:10 +0700

    optimize scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,12 @@ import './navbar'
 import './animations'
 import './sliders'
 import './ourway'
+
+
+if (history.scrollRestoration) {
+  history.scrollRestoration = 'manual';
+} else {
+  window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+  }
+}
